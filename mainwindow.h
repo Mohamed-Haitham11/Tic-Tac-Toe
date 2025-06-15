5ace621c82f1fece453fa0a484bd1a2b75aa0113
Mohamed-Haitham11 <mohamedhaitham2233@gmail.com>
2025-06-15 17:11:22 +0300

    d

--- mainwindow.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-
-// This header is required by the .pro file
-// but we're using TicTacToe as our main window class
-
-#endif // MAINWINDOW_H
